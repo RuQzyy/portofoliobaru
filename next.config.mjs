9b2b7f1779901e461fa11e58aa36d7125dfa5706
RuQzyy <Muhammadfaruq096@gmail.com>
2026-02-23 22:30:43 +0900

    fix basePath only

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
   basePath: "/portofoliobaru",
-  assetPrefix: "/portofoliobaru/",
-
   images: {
     unoptimized: true,
   },
